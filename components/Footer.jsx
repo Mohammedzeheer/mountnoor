50fbd98b9d77aa7ce39e607b7878ec6a95a0e586
Mohammed <zeheerzak@gmail.com>
2024-10-01 13:13:35 +0530

    updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,8 +7,8 @@ import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-lightYellow pt-44 pb-24 font-rubik shadow-lg">
-      <div className="max-w-screen-xl w-full mx-auto  px-12 lg:px-24 md:px-24  grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+    <div className="bg-lightYellow pt-40 pb-24 font-rubik shadow-lg">
+      <div className="max-w-screen-xl w-full mx-auto px-12 lg:px-24 md:px-24 grid grid-rows-5 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           <Image
             src={mountnoorLogo}
